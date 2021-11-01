b5593f3910990900480a970aaa01c64747f9588e
kevaf <kevinkipyegon20@gmail.com>
2021-11-02 00:03:00 +0300

    added index page

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -31,15 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hood',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'hood',
+    # 'crispy_forms',
+    
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
